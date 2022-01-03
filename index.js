53dc3e81ca4b00d2166d0d71daaf0a8f03bddf22
Alicia <arrowsrp@yahoo.com>
2022-01-03 11:53:00 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const body = document.getElementsByTagName('body')[0];
+
 
 function submitData (userName, userEmail) {
 
@@ -7,19 +7,43 @@ function submitData (userName, userEmail) {
         "email": userEmail
     }
 
-    fetch('http://localhost:3000/users', {
+    return fetch('http://localhost:3000/users', {
         method: "POST",
         headers: {
             'Content-Type': 'application/json',
-            Accept: 'application/json'
+            'Accept': 'application/json'
         },
         body: JSON.stringify(data)
     })
-    .then (res => res.json(data))
+    .then (res => res.json())
     .then (data => {
-        body.innerHTML = `${userName}, ${userEmail}`
+        document.body.innerHTML = `${data.id}`
     })
-    .catch (function(error){
-        body.innerHTML = 'Unauthorized Access'
-    })
-}
+    .catch ((error) => {
+        //console.log(error)
+        console.log(error.message)
+        document.body.innerHTML = error.message
+})}
+
+// function submitData( name, email ) {
+//     return fetch( 'http://localhost:3000/users', {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           "Accept": "application/json"
+//         },
+//         body: JSON.stringify( {
+//           name,
+//           email
+//         } )
+//       } )
+//       .then( function ( response ) {
+//         return response.json()
+//       } )
+//       .then( function ( object ) {
+//         document.body.innerHTML = object[ "id" ]
+//       } )
+//       .catch( function ( error ) {
+//         document.body.innerHTML = error.message
+//       } )
+//   }

355831866ebb8e4f7e18f3d22c5a4a4ee99f3c43
Alicia <arrowsrp@yahoo.com>
2022-01-03 10:54:56 -0700

    progress

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+const body = document.getElementsByTagName('body')[0];
+
+function submitData (userName, userEmail) {
+
+    let data = {
+        "name": userName,
+        "email": userEmail
+    }
+
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then (res => res.json(data))
+    .then (data => {
+        body.innerHTML = `${userName}, ${userEmail}`
+    })
+    .catch (function(error){
+        body.innerHTML = 'Unauthorized Access'
+    })
+}
